26118f49ee5d47548ec82cc66f12eb38d5b4d302
chadvidovcich <chad.vidovcich@gmail.com>
2022-09-13 16:41:09 -0700

    establish mongoDB connection

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const connectDB = require("./config/database.js");
+
+//Import routes
 const mainRoutes = require("./routes/main");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
+
+//Connect To Database
+connectDB();
 
 //Using EJS for views
 app.set("view engine", "ejs");
@@ -14,6 +23,18 @@ app.use(express.static("public"));
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: "hot dog app",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI 
+    }),
+  })
+);
 
 //Server Routes
 app.use("/",mainRoutes)
