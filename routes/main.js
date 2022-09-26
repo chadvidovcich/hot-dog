2aabb357fd578e085f8757e00397e76cc9114c2f
chadvidovcich <chad.vidovcich@gmail.com>
2022-09-26 09:28:38 -0700

    routes update

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home");
-const authController = require("../controllers/auth.js");
+const authController = require("../controllers/auth");
+const postsController = require("../controllers/posts");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes
 router.get("/", homeController.getIndex);
+router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
