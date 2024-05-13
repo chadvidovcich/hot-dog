21b110e306a5729bff8f8b6932795d21e53c764e
chadvidovcich <chad.vidovcich@gmail.com>
2024-05-13 11:23:56 -0700

    update port

--- server.js
+++ server.js
@@ -64,6 +64,6 @@ app.use('/post', postRoutes);
 app.use('/comment', commentRoutes);
 
 //Server Running
-app.listen(process.env.PORT, () => {
-	console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Server is running at http://localhost:${process.env.PORT || 3000}/ `);
 });
